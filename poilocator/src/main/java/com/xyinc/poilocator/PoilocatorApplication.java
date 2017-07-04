10362ca6a884314414465baa9e3b152f1abf2ec8
danil <danil@DESKTOP-6FLH08H>
2017-07-04 20:10:09 -0300

    initial commit

---
+++ poilocator/src/main/java/com/xyinc/poilocator/PoilocatorApplication.java
@@ -0,0 +1,12 @@
+package com.xyinc.poilocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoilocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoilocatorApplication.class, args);
+	}
+}
---
+++ poilocator/src/main/java/com/xyinc/poilocator/model/POI.java
@@ -0,0 +1,46 @@
+package com.xyinc.poilocator.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class POI implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	@Id @GeneratedValue
+	private long id;
+	private String nome;
+	private Integer coordenadaX;
+	private Integer coordenadaY;
+
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Integer getCoordenadaX() {
+		return coordenadaX;
+	}
+	
+	public void setCoordenadaX(Integer coordenadaX) {
+		this.coordenadaX = coordenadaX < 0 ? (coordenadaX*(-1)) : coordenadaX;
+	}
+	
+	public Integer getCoordenadaY() {
+		return coordenadaY;
+	}
+	
+	public void setCoordenadaY(Integer coordenadaY) {
+		this.coordenadaY = coordenadaY < 0 ? (coordenadaY*(-1)) : coordenadaY;
+	}
+}
