4d1cee6810e506554c734a9b8fa699f33d308410
danil <danil@DESKTOP-6FLH08H>
2017-07-06 11:43:22 -0300

    a welcome page was added to the api

--- poilocator/src/main/java/com/xyinc/poilocator/PoilocatorApplication.java
+++ poilocator/src/main/java/com/xyinc/poilocator/PoilocatorApplication.java
@@ -2,11 +2,21 @@ package com.xyinc.poilocator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class PoilocatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoilocatorApplication.class, args);
+	}
+	
+	@Controller 
+	public class HomeContoller {
+	    @RequestMapping("/")
+	    public String index() {
+	        return "index.html";
+	    }
 	}
 }
